bc59c18baf8181ee57888f4b7c968044fcb056e6
KJBurnett <burnettky@gmail.com>
2022-12-07 14:20:04 -0800

    Working download avatars and banners functionality with environment flags

--- avatarProcessor.py
+++ avatarProcessor.py
@@ -1,2 +1,43 @@
-def getChannelAvatarsAndBanners(channelName: str) -> str:
-    return "<method not implemented>"
+import os
+import subprocess
+
+# Internal modules
+import utils
+
+
+def getChannelAvatarsAndBanners(channelFolder: str, ytdlpProcessPath: str) -> None:
+    channelGuid = utils.getGuidFromTitle(channelFolder)
+    channelUrl = f"https://youtube.com/channel/{channelGuid}"
+
+    downloadAvatarsAndBannersFromChannel(channelFolder, channelUrl, ytdlpProcessPath)
+
+
+def downloadAvatarsAndBannersFromChannel(
+    channelFolder: str, channelUrl: str, ytdlpProcessPath: str
+) -> None:
+    downloadPath = os.path.join(channelFolder, "images")
+
+    if os.path.exists(downloadPath):
+        print("images/ already exists. Skipping.")
+        return  # If an images/ dir already exists, then we've already collected the avatars. Skip.
+    else:
+        print("Downloading avatars and banners from channel to images/")
+        os.mkdir(downloadPath)
+
+    projectPath = os.getcwd()
+
+    # Temporarily change the current working directory, to properly download the images.
+    os.chdir(downloadPath)
+
+    subprocess.run(
+        [
+            f"{ytdlpProcessPath}",
+            f"{channelUrl}",
+            "--write-all-thumbnails",
+            "--playlist-items",
+            "0",
+        ]
+    )
+
+    # Set the current working directory back to the project path.
+    os.chdir(projectPath)
