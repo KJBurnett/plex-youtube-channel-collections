ae38b8512196a5061f3995451dc3b60f0c1dc528
KJBurnett <burnettky@gmail.com>
2022-12-07 20:23:37 -0800

    More UTs, halfway done with main.py UTs. Should probably migrate to a utils folder or something

--- test_utils.py
+++ test_utils.py
@@ -11,8 +11,32 @@ def test_getGuidFromTitle_if_bad_title():
 
 
 def test_getGuidFromTitle_if_valid_title():
-    validTitle = "Z:\\Youtube\TheStradman [UC21Kozr_K0yDM-VjoihG9Aw]"
+    validTitle = "Z:\\Youtube\\TheStradman [UC21Kozr_K0yDM-VjoihG9Aw]"
 
     expectedResult = "UC21Kozr_K0yDM-VjoihG9Aw"
     result = utils.getGuidFromTitle(validTitle)
+    assert result == expectedResult
+
+
+def test_getChannelNameFromFolder():
+    bogusChannelFolder = "Z:\\Youtube\\TheStradman [UC21Kozr_K0yDM-VjoihG9Aw]"
+    expectedResult = "TheStradman"
+    result = utils.getChannelNameFromFolder(bogusChannelFolder)
+    assert result == expectedResult
+
+
+def test_getFileNamesFromDirectory_if_valid_directory(mocker):
+    bogusDirectory = "hiIAmBogus"
+    expectedResult = [
+        "Z:\\Youtube\\TheStradman [UC21Kozr_K0yDM-VjoihG9Aw]\\20120727 - Ferrari Dino 246 GTS - (81s) [qZbpzYNEziY].mkv"
+    ]
+    mocker.patch("utils.getFileNamesFromDirectory", return_value=expectedResult)
+    result = utils.getFileNamesFromDirectory(bogusDirectory)
+    assert result == expectedResult
+
+
+def test_getFileNamesFromDirectory_if_directory_is_not_valid():
+    bogusDirectory = "hiIAmBogusAndShouldNotExist"
+    expectedResult = []
+    result = utils.getFileNamesFromDirectory(bogusDirectory)
     assert result == expectedResult
