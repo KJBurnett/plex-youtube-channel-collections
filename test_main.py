742fa709bb5859327e5977490308423e0c858294
KJBurnett <burnettky@gmail.com>
2022-12-07 20:26:47 -0800

    commented out unfinished UT

--- test_main.py
+++ test_main.py
@@ -1,6 +1,7 @@
 import main
 from datetime import datetime
 
+
 # We expect the date to be prefixed to the title,
 # and it must be in YYYYMMDD format to work, as expected from yt-dlp.
 def test_getDateFromTitle_if_valid_title():
@@ -24,12 +25,12 @@ def test_getVideosFromChannelFolder_if_optimizeScans_is_false(mocker):
     assert result == expectedResult
 
 
-# TODO: Mock the functionality within optimizeScans=True
-def test_getVideosFromChannelFolder_if_optimizeScans_is_true(mocker):
-    bogusChannelFolder = "hiIAmBogus"
-    bogusVideoTitle = "Z:\\Youtube\\TheStradman [UC21Kozr_K0yDM-VjoihG9Aw]\\20120727 - Ferrari Dino 246 GTS - (81s) [qZbpzYNEziY].mkv"
-    bogusFilesList = [bogusVideoTitle]
-    expectedResult = ["20120727 - Ferrari Dino 246 GTS - (81s) [qZbpzYNEziY]"]
-    mocker.patch("utils.getFileNamesFromDirectory", return_value=bogusFilesList)
-    result = main.getVideosFromChannelFolder(bogusChannelFolder, optimizeScans=True)
-    assert result == expectedResult
+# # TODO: Mock the functionality within optimizeScans=True
+# def test_getVideosFromChannelFolder_if_optimizeScans_is_true(mocker):
+#     bogusChannelFolder = "hiIAmBogus"
+#     bogusVideoTitle = "Z:\\Youtube\\TheStradman [UC21Kozr_K0yDM-VjoihG9Aw]\\20120727 - Ferrari Dino 246 GTS - (81s) [qZbpzYNEziY].mkv"
+#     bogusFilesList = [bogusVideoTitle]
+#     expectedResult = ["20120727 - Ferrari Dino 246 GTS - (81s) [qZbpzYNEziY]"]
+#     mocker.patch("utils.getFileNamesFromDirectory", return_value=bogusFilesList)
+#     result = main.getVideosFromChannelFolder(bogusChannelFolder, optimizeScans=True)
+#     assert result == expectedResult
